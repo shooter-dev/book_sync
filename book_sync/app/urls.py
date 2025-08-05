0b60c081e756b1778a4b4c4e76c6cb8d49ea096e
sayana-project <sayana.ear@gmail.com>
2025-08-05 15:18:55 +0200

    update navbar

--- book_sync/app/urls.py
+++ book_sync/app/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 from .  import views
 
 urlpatterns = [
-    path("",views.index,name="index"),
+    path("home",views.index,name="index"),
+    path("research", views.research, name="research"),
+    path("collection", views.collection, name="collection"),
+    path("recommendation", views.recommendation, name="recommendation"),
 ]
