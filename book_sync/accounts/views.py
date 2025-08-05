2ed580afd2cea33da056b12e1f91550fb9716881
elvis messiaen <58810661+elvis-messiaen@users.noreply.github.com>
2025-08-05 15:36:11 +0200

    logout (#11)

--- book_sync/accounts/views.py
+++ book_sync/accounts/views.py
@@ -1,5 +1,19 @@
+from django.contrib.auth import logout
 from django.shortcuts import render
+from django.contrib import messages
+from django.shortcuts import render, redirect
 
 # Create your views here.
 def register_view(request):
     return render(request, 'register.html')
+
+
+def logout_view(request):
+    logout(request)
+    request.session.flush()
+
+    storage = messages.get_messages(request)
+    storage.used = True
+
+    messages.info(request, "Vous êtes maintenant déconnecté.")
+    return redirect('index')
