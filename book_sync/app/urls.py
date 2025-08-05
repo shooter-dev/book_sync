36fc372cb98b8546c862325b885dfebaa895e118
shooter-dev <vincentbleach@gmail.com>
2025-08-05 14:09:43 +0200

    feat: update core/urls create accounts/urls

--- book_sync/app/urls.py
+++ book_sync/app/urls.py
@@ -1,7 +1,6 @@
-from django.contrib import admin
 from django.urls import path
 from .  import views
 
 urlpatterns = [
-    path("",views.index,name="index"),
+    path("home",views.index,name="index"),
 ]
