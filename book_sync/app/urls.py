ba1d2e350aabe713a25b8fea7fc39f1d8bb5460f
shooterdev <31919820+shooter-dev@users.noreply.github.com>
2025-08-05 15:44:01 +0200

    update navbar (#12)
    Co-authored-by: sayana-project <sayana.ear@gmail.com>

--- book_sync/app/urls.py
+++ book_sync/app/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from .  import views
 
 urlpatterns = [
-    path("home",views.index,name="index"),
+    path("home", views.index, name="index"),
+    path("research", views.research, name="research"),
+    path("collection", views.collection, name="collection"),
+    path("recommendation", views.recommendation, name="recommendation"),
 ]
--- book_sync/app/views.py
+++ book_sync/app/views.py
@@ -3,3 +3,12 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+def research(request):
+    return render(request, 'research.html')
+
+def collection(request):
+    return render(request, 'collection.html')
+
+def recommendation(request):
+    return render(request, 'recommendation.html')
