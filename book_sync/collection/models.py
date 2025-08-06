3574bca04c9ea901cb3631656ab85cf324031810
shooter-dev <vincentbleach@gmail.com>
2025-08-06 14:49:27 +0200

    feat: create model Possession

--- book_sync/collection/models.py
+++ book_sync/collection/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 import uuid
 
 
@@ -51,14 +52,31 @@ class Volume(models.Model):
     title = models.CharField(max_length=255)
     number = models.IntegerField(default=1)
     release_date = models.DateField()
-    isbn = models.CharField()
+    isbn = models.CharField(max_length=13)
     possessions_count = models.IntegerField(default=0)
     image_url = models.TextField(default='cover.png')
     serie = models.ForeignKey(Serie, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return f"{self.serie.title} - Tome {self.number}: {self.title}"
 
     class Meta:
         verbose_name = "Volume"
         verbose_name_plural = "Volumes"
-        ordering = ['title', 'number']
-        
+        ordering = ['serie__title', 'number']
+
+
+class Possession(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    user = models.ForeignKey(User, on_delete=models.PROTECT, related_name='possessions')
+    volume = models.ForeignKey(Volume, on_delete=models.PROTECT, related_name='user_possessions')
+    ajouter_le = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user.username} possède {self.volume}"
+
+    class Meta:
+        verbose_name = "Possession"
+        verbose_name_plural = "Possessions"
+        unique_together = ('user', 'volume')
+        ordering = ['-ajouter_le']
