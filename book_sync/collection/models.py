572797eea6656d634d05750c57e90072dd5b82d1
shooter-dev <vincentbleach@gmail.com>
2025-08-06 11:32:42 +0200

    feat: create model Genre SCRUM-193

--- book_sync/collection/models.py
+++ book_sync/collection/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+import uuid
 
-# Create your models here.
+
+class Genre(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    title = models.CharField(max_length=255)
+    to_display = models.BooleanField(default=True)
+    
+    def __str__(self):
+        return self.title
+    
+    class Meta:
+        verbose_name = "Genre"
+        verbose_name_plural = "Genres"
+        ordering = ['title']
