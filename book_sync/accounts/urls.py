ad3b2e18169582fd1cf08abe2354ff0b6080778d
shooter-dev <vincentbleach@gmail.com>
2025-08-06 12:03:08 +0200

    feat: corection urls

--- book_sync/accounts/urls.py
+++ book_sync/accounts/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path("register/", views.register_view, name="register"),
     path("logout/", views.logout_view, name="logout"),
     path("profile/", views.profile_view, name="profile"),
-    path("subscribe", views.subscribe, name="subscribe"),
+    path("subscribe/", views.subscribe, name="subscribe"),
 ]
