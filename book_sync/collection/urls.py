e5164f7f82787429a3d36c88ccfd398046983978
shooterdev <31919820+shooter-dev@users.noreply.github.com>
2025-08-18 11:26:06 +0200

    feat: add page serie detail

--- book_sync/collection/urls.py
+++ book_sync/collection/urls.py
@@ -3,6 +3,7 @@ from .  import views
 from django.contrib import admin
 
 urlpatterns = [
-    path("search/", views.search, name="search"),path("search/", views.search, name="search"),
+    path("search/", views.search, name="search"),
     path("home", views.collection, name="collection"),
+    path("serie/<uuid:serie_id>/", views.serie_detail, name="serie_detail"),
 ]
