750e19a47e427de805b30c0cafe84aab5b2853c7
Elvis <elvism72@gmail.com>
2025-08-19 09:44:22 +0200

    delete volume

--- book_sync/collection/urls.py
+++ book_sync/collection/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path("home", views.collection, name="collection"),
     path("serie/<uuid:serie_id>/", views.serie_detail, name="serie_detail"),
     path("volume/<uuid:volume_id>/", views.volume_detail, name="volume_detail"),
-    path('add/<uuid:volume_id>/', views.add_collection, name='add_collection')
+    path('add/<uuid:volume_id>/', views.add_collection, name='add_collection'),
+    path('delete/<uuid:volume_id>/', views.delete_volume_collection, name='delete_volume_collection'),
 ]
