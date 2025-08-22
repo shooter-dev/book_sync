1e4a3b12b7a780abb4a88f5b01bb8e269e488073
Elvis <elvism72@gmail.com>
2025-08-22 16:08:37 +0200

    tempo genre

--- book_sync/prediction/urls.py
+++ book_sync/prediction/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import prediction_view, category_preference_view
+from .views import prediction_view, category_preference_view, genre_preference_view
 
 urlpatterns = [
     path("get-prediction/", prediction_view, name="get_prediction"),
     path("", category_preference_view, name="category_preference"),
+    path("genres/", genre_preference_view, name="genre_preference"),
 ]
--- book_sync/prediction/views.py
+++ book_sync/prediction/views.py
@@ -3,6 +3,8 @@ from django.shortcuts import render
 from django.http import JsonResponse
 import requests
 from collection.models import Kind
+from collection.models import like_kind
+from collection.models import Genre
 
 
 def prediction_view(request):
@@ -13,19 +15,15 @@ def prediction_view(request):
     except Exception as e:
         return JsonResponse({"error": str(e)}, status=500)
 
-
 def category_preference_view(request):
-    # user_kind_likes = like_kind.objects.filter(user=request.user).select_related('kind')
-    kinds = Kind.objects.all()
+    user_kind_likes = like_kind.objects.filter(user=request.user).select_related('kind')
+    kinds = [like.kind for like in user_kind_likes]
     return render(request, 'prediction.html', {'kinds': kinds})
 
 
+def genre_preference_view(request):
+    genres = Genre.objects.all()
+    print(f"Genres récupérés: {genres.count()}")
 
+    return render(request, 'prediction.html', {'genres': genres})
 
-
-
-
-#
-#def category_preference_view(request):
-#    kinds = Kind.objects.all()
- #   return render(request, 'prediction.html', {'kinds': kinds})
